4d84fed1633af7767f93fc984ccb69c5f9c2c598
Christian <jan514673@d204.lths.net>
2025-04-09 13:06:10 -0500

    Initial Commit

---
+++ OurFitnessPal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OurFitnessPal
+//
+//  Created by Janev, Christian (514673) on 4/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OurFitnessPal/OurFitnessPalApp.swift
@@ -0,0 +1,17 @@
+//
+//  OurFitnessPalApp.swift
+//  OurFitnessPal
+//
+//  Created by Janev, Christian (514673) on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct OurFitnessPalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
