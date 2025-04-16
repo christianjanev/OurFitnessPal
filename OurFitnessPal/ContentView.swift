f81d037827731f69cf12fa39922a3d87463e580b
Rolando <cah513355@d204.lths.net>
2025-04-16 13:08:49 -0500

    Style for overview 1 4/16

--- OurFitnessPal/ContentView.swift
+++ OurFitnessPal/ContentView.swift
@@ -11,13 +11,47 @@ struct ContentView: View {
     // nutritionix
     // cloudkit
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        VStack(){
+            
+            //Main Title
+            Text("OurFitenssPal")
+                .font(.system(size: 40))
+            
+            Text("Remain")
+                .font(.system(size: 30))
+
+            ZStack(){
+                
+                
+                Text("")
+                    .fixedSize(horizontal: false, vertical: true)
+                    .multilineTextAlignment(.center)
+                    .padding()
+                    .frame(width: 300, height: 200)
+                    .background(Rectangle().fill(Color.white).shadow(radius: 3))
+                
+                Text("Calories")
+                    .frame(width: 40, height: 40)
+                    .offset(x: 30, y: 40)
+                
+                Text("Protien")
+                
+                Text("Carbs")
+                
+                Text("Fats")
+
+
+                
+                
+                
+                
+            }
+            
+            
         }
-        .padding()
+
+        
     }
 }
 
