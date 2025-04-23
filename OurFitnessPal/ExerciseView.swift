5837b68eb3d58ce6d465f2c0a6162a9b2f1e384f
Christian <jan514673@d204.lths.net>
2025-04-23 12:18:22 -0500

    Persistence Data

--- OurFitnessPal/ExerciseView.swift
+++ OurFitnessPal/ExerciseView.swift
@@ -6,10 +6,27 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ExerciseView: View {
+    @Query var rawCalorieData: [CalorieData]
+    @Environment(\.modelContext) var modelContext
+    var calories: [Date: [CalorieData]] {
+        Dictionary(grouping: rawCalorieData) { calorie in
+            Calendar.current.startOfDay(for: calorie.date)
+        }
+    }
+    
     var body: some View {
-        Text("Exercise View")
+        VStack {
+            List {
+                ForEach(calories.keys.sorted(), id: \.self) { date in
+                    ForEach(calories[date]!, id: \.self) { calorie in
+                        Text(calorie.name)
+                    }
+                }
+            }
+        }
     }
 }
 
