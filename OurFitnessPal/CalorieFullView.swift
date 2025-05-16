d7d4a4be3d5b62a3ccb6109e43bd66ccf620c1e7
Rolando <cah513355@d204.lths.net>
2025-05-16 12:21:25 -0500

    name of food

--- OurFitnessPal/CalorieFullView.swift
+++ OurFitnessPal/CalorieFullView.swift
@@ -12,11 +12,109 @@ struct CalorieFullView: View {
     let calorie: CalorieData
     
     var body: some View {
+        Text(calorie.name)
+            .font(.largeTitle)
+        
         VStack {
-            Text(calorie.name)
-                .font(.largeTitle)
-                .frame(maxHeight: .infinity, alignment: .topLeading)
+            
+            Text("")
+            //HStack for Calories
+            HStack(){
+                Text("Calories")
+                    .font(.title)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 10)
+                //fill in backend number
+                Text("1909")
+                    .font(.title)
+                    .padding(.trailing, 10)
+            }
+            
+            //Divider
+            Rectangle()
+                .frame(width: 275, height: 20)
+            
+            HStack(){
+                Text("Total Fat:")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, 20)
+                
+                Text("45")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, -45)
+            }
+            HStack(){
+                Text("Saturated:")
+                    .frame(width: 275, alignment: .leading)
+                    .padding(.leading, 100)
+                
+            
+                
+            }
+            
+            HStack(){
+                Text("Unsaturated:")
+                    .frame(width: 275, alignment: .leading)
+                    .padding(.leading, 100)
+                
+            }
+            
+            
+            HStack(){
+                Text("Trans:")
+                    .frame(width: 275, alignment: .leading)
+                    .padding(.leading, 100)
+            }
+            
+            Text("")
+            
+            HStack(){
+                Text("Sodium:")
+                    .frame(width: 275, alignment: .leading)
+                    .padding(.leading, 20)
+                Text("45")
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.leading, -45)
+                
+            }
+            
+            HStack(){
+                Text("Carbs:")
+                    .frame(width: 275, alignment: .leading)
+                    .padding(.leading, 20)
+                
+            }
+            
+            HStack(){
+                Text("Sugar:")
+                    .frame(width: 275, alignment: .leading)
+                    .padding(.leading, 20)
+                
+
+            }
+            
+            HStack(){
+                Text("Protein:")
+                    .frame(width: 275, alignment: .leading)
+                    .padding(.leading, 20)
+
+            }
+            
+            HStack(){
+                Text("")
+            }
+
+            
+            
+            
         }
+        .frame(width: 275)
+        .border(.black)
+        .padding()
+        
+
+        
+        
     }
 }
 
